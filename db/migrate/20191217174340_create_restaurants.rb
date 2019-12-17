eff84302024831b66e039a45ef3a7e1951ea8480
Benjamin Bruyninx <bbruyninx@hotmail.com>
2019-12-17 19:47:13 +0100

    new routes and review model

---
+++ db/migrate/20191217174340_create_restaurants.rb
@@ -0,0 +1,15 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.string :category
+      t.text :content
+      t.integer :stars
+      t.text :references
+
+      t.timestamps
+    end
+  end
+end
